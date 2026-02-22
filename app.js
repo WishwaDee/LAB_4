e7518921cef660ac0445ea13cfa8566a856ba008
Savindu <savinduweerarathna@gmail.com>
2026-02-23 01:14:41 +0530

    Added a contribution message

--- app.js
+++ app.js
@@ -1,15 +1,2 @@
-let condition = true;
-const myPromise = new Promise((resolve, reject) => {
-    if (condition) { resolve('Success!'); } 
-    else { reject('Failure!'); }
-});
-
-async function myFunction() {
-    try {
-        const result = await myPromise;
-        console.log(result);
-    } catch (error) {
-        console.log(error);
-    }
-}
-myFunction();
+// Contributed by Savindu
+console.log("Hello from Savindu's side!");
